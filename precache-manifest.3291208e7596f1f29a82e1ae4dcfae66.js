e5e38dd8bf6b244b9acb65e13423e2f21f396946
Sergij Babich <moran.a@list.ru>
2020-03-02 20:22:20 +0200

    Updates

---
+++ precache-manifest.3291208e7596f1f29a82e1ae4dcfae66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1967f4818e6ccd1670de8b5b5bcf48",
+    "url": "/AUDD/index.html"
+  },
+  {
+    "revision": "63c6ef060a212d266ef9",
+    "url": "/AUDD/static/css/2.0683c493.chunk.css"
+  },
+  {
+    "revision": "60714ffccaed55814fbc",
+    "url": "/AUDD/static/css/main.d7de615e.chunk.css"
+  },
+  {
+    "revision": "63c6ef060a212d266ef9",
+    "url": "/AUDD/static/js/2.a99f8568.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AUDD/static/js/2.a99f8568.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60714ffccaed55814fbc",
+    "url": "/AUDD/static/js/main.1c6cc907.chunk.js"
+  },
+  {
+    "revision": "710da9d0bc8e45d627e2",
+    "url": "/AUDD/static/js/runtime-main.b32a98a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AUDD/static/media/logo.5d5d9eef.svg"
+  }
+]);
